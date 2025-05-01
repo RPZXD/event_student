ee892dcbe8acc214eca65b62356367359fa00de9
RPZXD <rpz.tiwa@phichai.ac.th>
2025-05-01 17:05:17 +0700

    Update EventController.php

--- controllers/EventController.php
+++ controllers/EventController.php
@@ -2,6 +2,9 @@
 session_start();
 
 header('Content-Type: application/json');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 require_once __DIR__ . '/../classes/DatabaseEvent.php';
 require_once __DIR__ . '/../classes/DatabaseUsers.php';
