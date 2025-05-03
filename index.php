f0dc79d7a7ac2710f67bb973950490e56a182e1b
RPZXD <rpz.tiwa@phichai.ac.th>
2025-05-03 16:08:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,6 @@ require_once('header.php');
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0"><?php echo $global['nameschool']; ?></h1>
           </div>
         </div>
       </div>
